70966eb46a8528993356dc6ed92d4a6738a36170
Betobaz <pedroalberto007@gmail.com>
2013-02-28 19:59:45 -0600

    jquery iniciando

---
+++ clase3/codiguito.js
@@ -0,0 +1,23 @@
+/*
+	Objetos de javascript
+	Browser 	-> navigator
+		Tiene:
+			- Manipulación de acelerometro
+	Ventana/Tab -> 	window
+		Tiene:
+			- tamaño de la ventana
+	Documento	->	document
+		Tiene:
+			- El DOM
+*/
+
+/*
+	jQuery es representado tambien por el alias $
+*/
+
+$(document).on("ready", inicio);
+
+function inicio()
+{
+	alert("Mama, ya se programar!")
+}
