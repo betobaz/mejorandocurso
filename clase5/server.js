fcbf7f39f28d3c2cdd58aeb0642ddfdb6fde24f1
Betobaz <pedroalberto007@gmail.com>
2013-03-06 19:29:37 -0600

    lista de mensajes

--- clase5/server.js
+++ clase5/server.js
@@ -1,14 +1,20 @@
 var express = require("express"),
-	app = express();
+	app = express(),
+	mensajes = [];
 
 app.get("/", function(req, res){
 	res.send("hellow world");
 });
 
-app.get("/mensajes/:mensaje", function(req, res){
+app.get("/mensajes/new/:mensaje", function(req, res){
 	// obtener los parametros con req.params
+	mensajes.push(req.params.mensaje);
 	res.send("Tu mensaje es :" + req.params.mensaje);
 });
+
+app.get("/mensajes/list", function(req, res){
+	res.send(mensajes);
+})
 
 app.listen(3000);
 console.log("Aplicacion funcionando");
