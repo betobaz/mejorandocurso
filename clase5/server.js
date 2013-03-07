0c205860a4eb5db9238cb039b55b6eb7ee4ed52f
Betobaz <pedroalberto007@gmail.com>
2013-03-06 19:27:08 -0600

    mensajes

--- clase5/server.js
+++ clase5/server.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+var express = require("express"),
+	app = express();
+
+app.get("/", function(req, res){
+	res.send("hellow world");
+});
+
+app.get("/mensajes/:mensaje", function(req, res){
+	// obtener los parametros con req.params
+	res.send("Tu mensaje es :" + req.params.mensaje);
+});
+
+app.listen(3000);
+console.log("Aplicacion funcionando");
